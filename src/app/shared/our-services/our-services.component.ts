ffb9f2d3fa5f174b8962b5283660772baa5a3750
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-07-22 21:59:55 +0700

    our service section

--- src/app/shared/our-services/our-services.component.ts
+++ src/app/shared/our-services/our-services.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPhoneSquare, faPhone } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-our-services',
   templateUrl: './our-services.component.html',
   styleUrls: ['./our-services.component.scss']
 })
 export class OurServicesComponent implements OnInit {
-
+  faPhone = faPhone;
   constructor() { }
 
   ngOnInit(): void {
