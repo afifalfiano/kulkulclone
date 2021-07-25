e25fff06a3c276b9a357b979040260511b169d79
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-07-25 08:33:08 +0700

    fix mobile

--- src/app/shared/why-kulkul/why-kulkul.component.ts
+++ src/app/shared/why-kulkul/why-kulkul.component.ts
@@ -1,3 +1,4 @@
+import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./why-kulkul.component.scss']
 })
 export class WhyKulkulComponent implements OnInit {
-
+  showCarousel = false;
   constructor() { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { innerWidth: any; }; }) {
+    console.log(event.target.innerWidth);
+    const width = event.target.innerWidth;
+    if (width < 426) {
+      this.showCarousel = true;
+    } else {
+      this.showCarousel = false;
+    }
+  }
+
 
   ngOnInit(): void {
   }
