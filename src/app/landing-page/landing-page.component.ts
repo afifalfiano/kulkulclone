7f0a89c26a2f452f6373e115cd6be1f99910218e
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-07-31 12:06:56 +0700

    fix

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
-
+export class LandingPageComponent implements OnInit, OnDestroy {
+  buttonTop: any;
   constructor() { }
+  ngOnDestroy(): void {
+    if(this.buttonTop) {
+      clearInterval(this.buttonTop);
+    }
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    this.hiddenButton();
+    this.buttonTop = setInterval(
+      () => {
+        this.hiddenButton();
+      }, 1000
+    )
+}
+
+hiddenButton() {
+    const ontop = document.getElementById('ontop');
+      const {y} = document.body.getBoundingClientRect();
+      if (y > -700) {
+        if (ontop) {
+          ontop.style.visibility = 'hidden';
+        }
+    } else {
+      if(ontop) {
+        ontop.style.visibility = 'visible';
+      }
+    }
+}
+
+  onTop() {
+    const top = document.querySelector('#top');
+    const hero = document.querySelector('#hero-comp');
+    
+      if (top) {
+        top.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      }
   }
 
 }
